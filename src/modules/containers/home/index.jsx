fde1b760e781a55ab01cf9e45c52e006b1dabc11
Walter <36119371+Hwenze@users.noreply.github.com>
2022-10-18 17:51:28 +0800

    首页模块补充

--- src/modules/containers/home/index.jsx
+++ src/modules/containers/home/index.jsx
@@ -1,20 +1,24 @@
 /*
  * @Author: HWZ
  * @Date: 2022-07-04
- * @LastEditTime: 2022-10-14 17:11:50
+ * @LastEditTime: 2022-10-18 17:50:50
  * @LastEditors: HWZ
  * @Description: 公共头部
  */
+import { useNavigate } from 'react-router-dom';
 
-import { fullmeasureList, newsList } from './models';
+import { fullmeasureList, newsList, honorList } from './models';
 
 import './index.scss';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div id='Home'>
       {/* 轮播图 */}
       <div className='banner'></div>
+
       {/* 平台介绍 */}
       <div className='fullmeasureOuterContent'>
         {fullmeasureList.map((item) => {
@@ -36,13 +40,24 @@ const Home = () => {
         <div className='content-box'>
           {newsList.map((item) => {
             return (
-              <div className='content-list'>
+              <div className='content-list' key={item.title}>
                 <img src={item.img} alt='' />
                 <p>{item.title}</p>
                 <span>{item.text}</span>
               </div>
             );
           })}
+        </div>
+      </div>
+
+      {/* 社区 */}
+      <div className='community'>
+        <p className='community-title'>社区</p>
+        <p className='community-info'>
+          我们希望为提供一个纯粹、高质的技术交流的平台，与一起学习、交流与成长
+        </p>
+        <div className='community-btn' onClick={() => navigate('/contact')}>
+          了解详情
         </div>
       </div>
     </div>
--- src/modules/containers/home/models.js
+++ src/modules/containers/home/models.js
@@ -15,6 +15,9 @@ import data from '../../../static/images/data.png';
 import news1 from '../../../static/images/news1.png';
 import news2 from '../../../static/images/news2.png';
 import news3 from '../../../static/images/news3.png';
+import honor1 from '../../../static/images/2014.png';
+import honor2 from '../../../static/images/2016.png';
+import honor3 from '../../../static/images/2018.png';
 
 export const fullmeasureList = [
     { img: personal, title: '个人用户', text: '模块制定、众创定制、专属定制满足您的多种个性化需求' },
@@ -26,4 +29,10 @@ export const newsList = [
     { img: news1, title: '企业IT规划发展的三个时期', text: 'IT规划不是摆设，它必须在得到切实可行的实施之后才能发挥出促进业务、提升管理的作用，否则就是一堆废纸。因此，“落地' },
     { img: news2, title: '前苹果高管: 微软本可避免WP失败', text: '日前，Medium专栏作者、前苹果高管Jean-Louis Gassée在一篇关于微软Windows Phone的' },
     { img: news3, title: '看3D电影能让你的大脑更灵活', text: '看3D电影让人享受新奇的视觉体验，英国伦敦戈德史密斯大学一项研究显示，这还有助脑力提升研究人员招募100多名志愿者' },
+];
+
+export const honorList = [
+    { img: honor1, text: '2014XXX互联网年度产品' },
+    { img: honor2, text: '2016XXX互联网年度产品' },
+    { img: honor3, text: '2018XXX互联网年度产品' }
 ];
