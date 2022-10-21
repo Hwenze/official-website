e4389df7da8aba50f5fb0d12fab64d8b9cd3b30e
GGSDDU <592088039@qq.com>
2022-10-21 16:02:55 +0800

    联系加盟

---
+++ src/modules/containers/gallery/model.js
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion: 
+ * @Author: yqy
+ * @Date: 2022-10-17 14:12:00
+ * @LastEditors: hzy
+ * @LastEditTime: 2022-10-20 08:44:52
+ */
+
+import gallery1 from '../../../static/images/2014.png';
+import gallery2 from '../../../static/images/2016.png';
+import gallery3 from '../../../static/images/2018.png';
+
+// 模式说明
+export const modelData = [
+    {img: gallery1, type: '2018XXX中国年度互联网优秀奖'},
+    {img: gallery2, type: '2014XXX互联网年度产品优秀奖'},
+    {img: gallery3, type: '2016XXX互联网年度杰出奖'}
+];
